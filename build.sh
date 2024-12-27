b49ef93bc635d649ab22feebb2b030105a820410
Keith Hubbard <keith@boxfan.org>
2024-12-26 19:36:49 -0500

    Switch to nightly Emacs build

--- build.sh
+++ build.sh
@@ -4,20 +4,8 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
+curl --retry 3 -Lo /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:alternateved:nightly-emacs.repo https://copr.fedorainfracloud.org/coprs/alternateved/nightly-emacs/repo/fedora-${RELEASE}/alternateved-nightly-emacs-fedora-${RELEASE}.repo
 
-### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
 rpm-ostree install emacs
 
-# this would install a package from rpmfusion
-# rpm-ostree install vlc
-
-#### Example for enabling a System Unit File
-
-# systemctl enable podman.socket
+systemctl enable podman.socket
