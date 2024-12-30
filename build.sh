4de2f24fe08313f87ac9c1b2714db76a9e2b4a7f
Keith Hubbard <keith@boxfan.org>
2024-12-30 12:03:04 -0500

    Try to get Emacs vterm working

--- build.sh
+++ build.sh
@@ -4,8 +4,10 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
-curl --retry 3 -Lo /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:alternateved:nightly-emacs.repo https://copr.fedorainfracloud.org/coprs/alternateved/nightly-emacs/repo/fedora-${RELEASE}/alternateved-nightly-emacs-fedora-${RELEASE}.repo
+curl --retry 3 -Lo \
+     /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:alternateved:nightly-emacs.repo \
+     https://copr.fedorainfracloud.org/coprs/alternateved/nightly-emacs/repo/fedora-${RELEASE}/alternateved-nightly-emacs-fedora-${RELEASE}.repo
 
-rpm-ostree install emacs
+rpm-ostree install emacs cmake libtool
 
 systemctl enable podman.socket
