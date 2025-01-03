9b2bf7c07e5743fb1774976d36db21e12c18b31b
Keith Hubbard <keith@boxfan.org>
2025-01-03 16:36:30 -0500

    Add Wezterm nightly

--- build.sh
+++ build.sh
@@ -4,10 +4,16 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
-curl --retry 3 -Lo \
-     /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:alternateved:nightly-emacs.repo \
+curl --retry 3 -L \
+     -o /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:alternateved:nightly-emacs.repo \
      https://copr.fedorainfracloud.org/coprs/alternateved/nightly-emacs/repo/fedora-${RELEASE}/alternateved-nightly-emacs-fedora-${RELEASE}.repo
 
-rpm-ostree install emacs cmake libtool
+curl --retry 3 -L \
+     -o /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:wezfurlong:wezterm-nightly.repo \
+     https://copr.fedorainfracloud.org/coprs/wezfurlong/wezterm-nightly/repo/fedora-${RELEASE}/wezfurlong-wezterm-nightly-fedora-${RELEASE}.repo
+
+rpm-ostree install \
+    emacs cmake libtool \
+    wezterm
 
 systemctl enable podman.socket
